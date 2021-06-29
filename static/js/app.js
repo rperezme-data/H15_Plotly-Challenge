336e8e9abc63bb578a31cca3520ace91138c9b98
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-06-28 22:09:26 -0500

    Render Dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,18 @@
+// UNPACK FUNCTION
+function unpack(rows, index) {
+    return rows.map(function (row) {
+        return row[index];
+    });
+}
+
+
+// SELECT ELEMENTS (dropdown)
+var dropdown = d3.select("#selDataset");
+
+// Q1: HTML inline handling??
+// HANDLE ELEMENTS (dropdown)
+// dropdown.on('change', optionChanged);
+
 
 // LOAD DATA
 // Read JSON dataset using D3 library (samples.json)
@@ -8,9 +23,25 @@ d3.json("data/samples.json").then((data) => {
     var metadata = data.metadata;
     var samples = data.samples;
 
+    // DEBUG
+    console.log(data);
+    console.log(names);
+    console.log(metadata);
+    console.log(samples);
+
+    // Render dropdown (sample names)
+    renderDrowpdown(names);
+
 })
 
-// DEBUG
-console.log(names);
-console.log(metadata);
-console.log(samples);
+
+// RENDER DROPDOWN FUNCTION
+function renderDrowpdown(names) {
+    // Render dropdown options
+    names.forEach(name => {
+        dropdown.append('option').text(name);
+        console.log(`Rendered dropdown`);   //DEBUG
+    });
+}
+
+
