87607364220f7c07eba1e77c67e002f0b8c8d585
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-06-28 23:30:44 -0500

    Render Metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,7 @@
+
+// Q0: Uncaught SyntaxError: Unexpected token ':'
+
+
 // UNPACK FUNCTION
 function unpack(rows, index) {
     return rows.map(function (row) {
@@ -8,6 +12,7 @@ function unpack(rows, index) {
 
 // SELECT ELEMENTS (dropdown)
 var dropdown = d3.select("#selDataset");
+var demographicInfo = d3.select("#sample-metadata")
 
 // Q1: HTML inline handling??
 // HANDLE ELEMENTS (dropdown)
@@ -20,6 +25,7 @@ d3.json("data/samples.json").then((data) => {
 
     // Assign data to variables
     var names = data.names;
+    // var names = data.names.map((row) => parseInt(row));
     var metadata = data.metadata;
     var samples = data.samples;
 
@@ -33,7 +39,51 @@ d3.json("data/samples.json").then((data) => {
     renderDrowpdown(names);
 
     // Default init()
-    dropdown.property('value', 945);   // Default value for plot
+    console.log(Math.min(names));   // Q3: What??
+    var defaultName = 940;
+    dropdown.property('value', defaultName);   // Default value for plot
+
+    // 
+
+    // HORIZONTAL BAR CHART
+
+    var defaultSample = samples.find(sample => parseInt(sample.id) === defaultName);
+    console.log("Default sample: ");
+    console.log(defaultSample);
+
+    function sortDesc(a, b) {
+        return b - a;
+    }
+
+    var topValues = defaultSample.sample_values.sort(sortDesc).slice(0, 10);
+    console.log("Top 10 values: ");
+    console.log(topValues);
+
+    topIds = [];
+    topLabels = [];
+
+    // defaultSample.find(row => row.sample_values === topValues[0]);
+
+    // for (var i = 1; i < topValues.length; i++) {
+    //     topIds.push(defaultSample.find(row.sample_values === topValues[i]));
+    // }
+    // var topIds = defaultSample.find(row => row.sample_values === topValue)
+    // var topLabels = 
+
+    // console.log(topIds);
+
+
+    var defaultMetadata = metadata.find(metadata => metadata.id === defaultName);
+    
+    console.log("Metadata: ");
+    console.log(defaultMetadata);
+    
+
+    for (const [key, value] of Object.entries(defaultMetadata)) {
+        demographicInfo.append('div').text(`${key.toUpperCase()}: ${value}`);
+      }
+
+
 
 })
 
